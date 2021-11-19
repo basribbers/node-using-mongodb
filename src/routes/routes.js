be238398153dc1a8378e8f722e5bb3a0519778da
basribbers <basribbers@gmail.com>
2021-11-19 15:45:36 +0100

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 import  { addnewProduct } from '../controllers/controllers';
 
 const routes = (app) => {
-app.routes('/products').post(addnewProduct);
+app.routes('/products')
+
+.post(addnewProduct);
 
 }
 export default routes;
