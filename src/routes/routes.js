d10887bf1573718d89b4d10659071317696f17c5
basribbers <basribbers@gmail.com>
2021-11-20 13:06:25 +0100

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
-import  { addnewProduct } from '../controllers/controllers';
-import { getProducts } from '../controllers/controllers';
-import { getProductWithID } from  '../controllers/controllers';
+import  { addnewProduct, getProducts, getProductWithID} from '../controllers/controllers';
 
 const routes = (app) => {
 app.route('/products')
 // Post endpoint
 .post(addnewProduct)
+//get multiple products
 .get(getProducts);
 
 app.route('/products/:ProductID')
+//get a specific product with ID
 .get(getProductWithID);
 
 }
