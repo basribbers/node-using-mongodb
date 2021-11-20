27aee8a3485553635e4a110497a6c29150458142
basribbers <basribbers@gmail.com>
2021-11-20 13:11:33 +0100

    Update controllers.js

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,31 +1,32 @@
 import mongoose from 'mongoose';
 import { ProductSchema} from '../models/models'; 
 
-
 const Product = mongoose.model('Product',ProductSchema);
-
 
 export const addnewProduct = (req, res) => { let newProduct = new Product(req.body);
 
     newProduct.save((err, Product) => {
-        if (err)
+        if (err){
           res.send(err);
+        }
         res.json(Product);
       });
 
     }
 
 export const getProducts = (req,res) => { Product.find((err,Product) => {
-  if (err)
+  if (err){
   res.send(err);
+  }
 res.json(Product);
 });
 }
 
 export const getProductWithID = (req, res) => {
   Product.findById(req.params.ProductID, (err,Product) => {
-  if (err)
+  if (err){
   res.send(err);
+  }
 res.json(Product);
 });
 }
